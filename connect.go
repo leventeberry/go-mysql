c49f8a9f9849d0f698b0507f24bb892fe3f5a330
leventeberry <berrylevente@gmail.com>
2025-03-20 13:26:15 -0400

    environment variable check functionality added

--- connect.go
+++ connect.go
@@ -9,6 +9,34 @@ import (
 
 // ConnectDB initializes the MySQL connection and returns a *sql.DB instance.
 func ConnectDB(net string, addr string, db_name string) (*sql.DB, error) {
+	if os.Getenv("DB_USER") == "" {
+		var user string;
+		fmt.Println("DB_USER environment variable not set.")
+		fmt.Println("What is the username for the database? (default: root): ")
+		fmt.Scanln(&user);
+		if user == "" {
+			user = "root";
+		} else {
+			os.Setenv("DB_USER", user);
+		}
+	} else {
+		fmt.Println("DB_USER environment variable set to:", os.Getenv("DB_USER"))
+	}
+
+	if os.Getenv("DB_PASS") == "" {
+		var pass string;
+		fmt.Println("DB_PASS environment variable not set.")
+		fmt.Println("What is the password for the database? (default: password): ")
+		fmt.Scanln(&pass);
+		if pass == "" {
+			pass = "password";
+		} else {
+			os.Setenv("DB_PASS", pass);
+		}
+	} else {
+		fmt.Println("DB_PASS environment variable set to:", os.Getenv("DB_PASS"))
+	}
+
 	// Configure database connection using environment variables
 	cfg := mysql.Config{
 		User:   os.Getenv("DB_USER"),
